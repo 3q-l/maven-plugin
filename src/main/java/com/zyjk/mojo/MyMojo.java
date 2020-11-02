0ebaa62aa5d3207814d69fe994cc333bd8e33c3b
shuaipengli <shuaipengli@91jkys.com>
2020-11-02 13:32:21 +0800

    +:first

---
+++ src/main/java/com/zyjk/mojo/MyMojo.java
@@ -0,0 +1,21 @@
+package com.zyjk.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * 第一个mojo
+ * @author lishuaipeng
+ * @date 2020/11/2 11:20 上午
+ */
+@Mojo(name = "hello")
+public class MyMojo extends AbstractMojo {
+
+
+
+    @Override
+    public void execute() throws MojoExecutionException {
+        getLog().info("==========Hello=============");
+    }
+}
